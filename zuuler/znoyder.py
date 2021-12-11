ee9340e2c55edaf81598f65c9acb3b46217ba295
Szymon Datko <sdatko@redhat.com>
2021-12-11 01:19:35 +0100

    Refactor znoyder
    We would like to use znoyder as part of other Python tools.
    Therefore it would be convenient if its methods return values
    we can process easier then, instead of running subprocess
    and parsing its output. Now regular import is possible.
    
    After refactor there is still a possibility to run the znoyder
    command line interface to browse OSP info base and filter
    for specific fields values directly in terminal.

--- zuuler/znoyder.py
+++ zuuler/znoyder.py
@@ -29,127 +29,123 @@ RDOINFO_GIT_URL = 'https://code.engineering.redhat.com/gerrit/ospinfo'
 APP_DESCRIPTION = 'Find OSP packages, repositories, components and releases.'
 
 
-def process_arguments():
-    parser = argparse.ArgumentParser(description=APP_DESCRIPTION)
-    subparsers = parser.add_subparsers(dest='command', metavar='command')
-
-    components = subparsers.add_parser('components', help='')
-    components.add_argument('--name', dest='name')
-
-    packages = subparsers.add_parser('packages', help='')
-    packages.add_argument('--component', dest='component')
-    packages.add_argument('--name', dest='name')
-    packages.add_argument('--tag', dest='tag')
-    packages.add_argument('--upstream', dest='upstream')
-    packages.add_argument('--output', dest='output',
-                          default='osp-name,osp-distgit,osp-patches',
-                          help='comma-separated list of fields to return')
-    packages.add_argument('--header', dest='header',
-                          default=False, action='store_true',
-                          help='print header with output names on top')
-    packages.add_argument('--debug', dest='debug',
-                          default=False, action='store_true',
-                          help='print all fields in output')
-
-    releases = subparsers.add_parser('releases', help='')
-    releases.add_argument('--tag', dest='tag')
-    releases.add_argument('--output', dest='output',
-                          default='ospinfo_tag_name,git_release_branch',
-                          help='comma-separated list of fields to return')
-    releases.add_argument('--header', dest='header',
-                          default=False, action='store_true',
-                          help='print header with output names on top')
-    releases.add_argument('--debug', dest='debug',
-                          default=False, action='store_true',
-                          help='print all fields in output')
-
-    args = parser.parse_args()
-
-    if not args.command:
-        parser.print_help()
-        sys.exit(1)
-
-    return args
+def get_distroinfo():
+    return di.DistroInfo(info_files=INFO_FILE,
+                         cache_ttl=24*60*60,  # 1 day in seconds
+                         remote_git_info=RDOINFO_GIT_URL).get_info()
 
 
-def process_components(info, args):
+def get_components(**kwargs):
+    info = get_distroinfo()
     components = info.get('components')
 
-    if args.name:
+    if kwargs.get('name'):
         components = [component for component in components
-                      if args.name == component.get('name')]
+                      if kwargs.get('name') == component.get('name')]
 
-    for component in components:
-        print(component.get('name'))
+    return components
 
 
-def process_packages(info, args):
+def get_packages(**kwargs):
+    info = get_distroinfo()
     packages = info.get('packages')
 
     packages = [package for package in packages
                 if 'osp-name' in package.keys()]
 
-    if args.component:
+    if kwargs.get('component'):
         packages = [package for package in packages
-                    if args.component == package.get('component')]
-    if args.name:
+                    if kwargs.get('component') == package.get('component')]
+    if kwargs.get('name'):
         packages = [package for package in packages
-                    if args.name == package.get('name')]
-    if args.tag:
+                    if kwargs.get('name') == package.get('name')]
+    if kwargs.get('tag'):
         packages = [package for package in packages
-                    if args.tag in package.get('tags')]
-    if args.upstream:
+                    if kwargs.get('tag') in package.get('tags')]
+    if kwargs.get('upstream'):
         packages = [package for package in packages
-                    if args.upstream in package.get('upstream')]
+                    if kwargs.get('upstream') in package.get('upstream')]
 
-    if args.debug:
-        pp = PrettyPrinter()
-        pp.pprint(packages)
-        return
-
-    output = [entry.strip() for entry in args.output.split(',')]
-    if args.header:
-        print(' '.join(output))
-        print(' '.join(['-' * len(field) for field in output]))
-
-    for package in packages:
-        print(' '.join([package.get(key, 'None') for key in output]))
+    return packages
 
 
-def process_releases(info, args):
+def get_releases(**kwargs):
+    info = get_distroinfo()
     releases = info.get('osp_releases')
 
-    if args.tag:
+    if kwargs.get('tag'):
         releases = [release for release in releases
-                    if args.tag in release.get('ospinfo_tag_name')]
+                    if kwargs.get('tag') in release.get('ospinfo_tag_name')]
 
-    if args.debug:
-        pp = PrettyPrinter()
-        pp.pprint(releases)
-        return
+    return releases
 
-    output = [entry.strip() for entry in args.output.split(',')]
-    if args.header:
-        print(' '.join(output))
-        print(' '.join(['-' * len(field) for field in output]))
 
-    for release in releases:
-        print(' '.join([release.get(key, 'None') for key in output]))
+def process_arguments():
+    parser = argparse.ArgumentParser(description=APP_DESCRIPTION)
+    subparsers = parser.add_subparsers(dest='command', metavar='command')
+
+    common = argparse.ArgumentParser(add_help=False)
+    common.add_argument('--debug', dest='debug',
+                        default=False, action='store_true',
+                        help='print all fields in output')
+    common.add_argument('--header', dest='header',
+                        default=False, action='store_true',
+                        help='print header with output names on top')
+    common.add_argument('--output', dest='output',
+                        help='comma-separated list of fields to return')
+
+    components = subparsers.add_parser('components', help='', parents=[common])
+    components.add_argument('--name', dest='name')
+
+    packages = subparsers.add_parser('packages', help='', parents=[common])
+    packages.add_argument('--component', dest='component')
+    packages.add_argument('--name', dest='name')
+    packages.add_argument('--tag', dest='tag')
+    packages.add_argument('--upstream', dest='upstream')
+
+    releases = subparsers.add_parser('releases', help='', parents=[common])
+    releases.add_argument('--tag', dest='tag')
+
+    arguments = parser.parse_args()
+
+    if not arguments.command:
+        parser.print_help()
+        sys.exit(1)
+
+    return arguments
 
 
 def main():
     args = process_arguments()
 
-    info = di.DistroInfo(info_files=INFO_FILE,
-                         cache_ttl=24*60*60,  # 1 day in seconds
-                         remote_git_info=RDOINFO_GIT_URL).get_info()
-
     if args.command == 'components':
-        process_components(info, args)
+        results = get_components(**vars(args))
+        default_output = ['name']
     elif args.command == 'packages':
-        process_packages(info, args)
+        results = get_packages(**vars(args))
+        default_output = ['osp-name', 'osp-distgit', 'osp-patches']
     elif args.command == 'releases':
-        process_releases(info, args)
+        results = get_releases(**vars(args))
+        default_output = ['ospinfo_tag_name', 'git_release_branch']
+    else:
+        results = None
+
+    if args.debug:
+        pp = PrettyPrinter()
+        pp.pprint(results)
+        return
+
+    if args.output:
+        output = [entry.strip() for entry in args.output.split(',')]
+    else:
+        output = default_output
+
+    if args.header:
+        print(' '.join(output))
+        print(' '.join(['-' * len(field) for field in output]))
+
+    for result in results:
+        print(' '.join([result.get(key, 'None') for key in output]))
 
 
 if __name__ == '__main__':
